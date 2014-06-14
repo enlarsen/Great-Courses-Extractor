1af36e3da590873bb4659a937c6ff7803d28002b
Erik Larsen <erik_larsen@icloud.com>
2014-06-14 11:15:34 -0700

    Initial Commit

---
+++ Great-Courses-Extractor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Great-Courses-Extractor
+//
+//  Created by Erik Larsen on 6/14/14.
+//  Copyright (c) 2014 Erik Larsen. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Great-Courses-Extractor/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Great-Courses-Extractor
+//
+//  Created by Erik Larsen on 6/14/14.
+//  Copyright (c) 2014 Erik Larsen. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
